1a8170b853097a86dc511f7f6f406bd7dc9f4c92
Vitor shermon <victorshermon@yahoo.com>
2026-09-10 19:02:02 -0300

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/shared/config/test/setup-tests.ts',
+    globals: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
